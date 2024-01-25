71d5d16e6f141fca33f86eb9ec3a4c51c4fdd93e
zoraethia <jonroasonfalck1@gmail.com>
2024-01-25 13:05:50 +0100

    Cleanup

--- fashion_mnist_project/models/cross_validation.py
+++ fashion_mnist_project/models/cross_validation.py
@@ -2,9 +2,6 @@ import numpy as np
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import OneHotEncoder
 from tensorflow.keras.datasets import fashion_mnist
-from tensorflow.keras import layers, models
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import CategoricalCrossentropy
 import keras_tuner as kt
 import hp_tuning
 
@@ -13,11 +10,11 @@ def load_hyperparameters(n=3):
     convolutional_hyperparameters = kt.GridSearch(hp_tuning.HyperModel(), objective='val_accuracy',
                                                   directory='hyperparameter_tuning_output_convolutional_layers',
                                                   project_name='fashion_mnist',
-                                                  overwrite=False).get_best_hyperparameters(num_trials=n)[0:n+1]
+                                                  overwrite=False).get_best_hyperparameters(num_trials=n)[0:n + 1]
     dense_hyperparameters = kt.GridSearch(hp_tuning.HyperModel(), objective='val_accuracy',
                                           directory='hyperparameter_tuning_output_dense_layers',
                                           project_name='fashion_mnist',
-                                          overwrite=False).get_best_hyperparameters(num_trials=n)[0:n+1]
+                                          overwrite=False).get_best_hyperparameters(num_trials=n)[0:n + 1]
 
     return convolutional_hyperparameters, dense_hyperparameters
 
@@ -45,43 +42,9 @@ def cross_validation(k=5):
                                      project_name='fashion_mnist',
                                      overwrite=False)
 
-
     CVTuner.search(x_train, y_train)
 
     best_hps = CVTuner.get_best_hyperparameters(num_trials=1)[0]
 
-    convolutional_layer_params = convolutional_layer_hyperparameters[best_hps.values['Convolutional Layers']]
-    dense_layer_params = dense_layer_hyperparameters[best_hps.values['Dense Layers']]
-    learning_rate = best_hps.values['Learning Rate']
+    print(best_hps.values)
 
-    model = models.Sequential()
-
-    for layer_num in range(convolutional_layer_params.values['num_convpool_layers']):
-        if layer_num == 0:
-            model.add(
-                layers.Conv2D(convolutional_layer_params.values[f'num_filters_{layer_num}'],
-                              kernel_size=convolutional_layer_params.values[f'filter_size_{layer_num}'],
-                              activation='relu', input_shape=(28, 28, 1)))
-        else:
-            model.add(
-                layers.Conv2D(convolutional_layer_params.values[f'num_filters_{layer_num}'],
-                              kernel_size=convolutional_layer_params.values[f'filter_size_{layer_num}'],
-                              activation='relu'))
-        model.add(layers.MaxPooling2D((2, 2)))
-
-    model.add(layers.Flatten())
-
-    for layer_num in range(dense_layer_params.values['num_dense_layers']):
-        model.add(
-            layers.Dense(units=dense_layer_params.values[f'num_units_{layer_num}'],
-                         activation='relu'))
-
-    model.add(layers.Dense(10))
-
-    model.compile(optimizer=Adam(learning_rate=learning_rate),
-                    loss=CategoricalCrossentropy(from_logits=True),
-                    metrics=['accuracy'])
-
-    model.save('models/final_model_for_real.keras')
-
-cross_validation(5)
